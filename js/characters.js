d7ff8ac90d1ad17d0ab51e174bf293f5e82b88a9
Eilert Vetle Forland <ev.forland@gmail.com>
2020-08-24 18:16:30 +0200

    added BEM to cards and styled them with dark background and yellow borders like the design. Added a header partial to create designated design for the header in its own partial

--- js/characters.js
+++ js/characters.js
@@ -1,4 +1,4 @@
-fetch("api/characters.json")
+fetch("https://raw.githubusercontent.com/FrontEndVetle/GOT_Game_SemesterProject2/master/api/characters.json")
     .then(function(response) {
         return response.json();
     })
@@ -16,19 +16,19 @@ function loopThroughCharacters(characters) {
     var characterDisplay = document.querySelector(".card-container");
     for (var i = 0; i < characters.length; i++) {
         characterDisplay.innerHTML +=
-            `<div class="col-xs-12 col-md-4"><div class="card">
-            <img class="card-img-top" src="${characters[i].Banner}" alt="Card image cap">
+            `<div class="col-xs-12 col-md-3"><div class="card cards">
+            <img class="card-img-top cards__img" src="${characters[i].Banner}" alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">${characters[i].Name}</h5>
+                <h5 class="card-title cards__title">${characters[i].Name}</h5>
             </div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">Born: ${characters[i].Born}</li>
-                <li class="list-group-item">Culture: ${characters[i].Culture} </li>
-                <li class="list-group-item">Alias: ${characters[i].Aliases[0]}</li>
-                 <li class="list-group-item"><img class="card-img-top" src="${characters[i].Token}" alt="Card image cap"></li>
+                <li class="list-group-item cards__info">Born: ${characters[i].Born}</li>
+                <li class="list-group-item cards__info">Culture: ${characters[i].Culture} </li>
+                <li class="list-group-item cards__info">Alias: ${characters[i].Aliases[0]}</li>
+                 <li class="list-group-item cards__info"><img class="card-img-top cards__token" src="${characters[i].Token}" alt="Card image cap"></li>
             </ul>
             <div class="card-body">
-                <a href="#" class="btn btn-primary">SELECT CHARACTER</a>
+                <a href="#" class="btn btn-primary cards__btn">SELECT CHARACTER</a>
             </div>
             </div>
             </div>`;
