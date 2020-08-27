850ee4804f2393526caedc87d9ae7ec56d394156
Eilert Vetle Forland <ev.forland@gmail.com>
2020-08-27 16:22:27 +0200

    added incremental movement to player in console.log for each press of the roll dice button in game.js on the game.html page.

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
+var player = JSON.parse(localStorage.getItem("player"));
+var com = JSON.parse(localStorage.getItem("com"));
+
 function createPlayer() {
     var player = JSON.parse(localStorage.getItem("player"));
-    console.log(player);
-
     let playerDisplay = document.querySelector(".player");
     playerDisplay.innerHTML += `
     <div class="card cards">
@@ -32,9 +33,54 @@ function createCom() {
 }
 createCom();
 
-//dice roll
+//dice roll and and token movement
 window.rollDice = () => {
     const max = 6;
     const roll = Math.ceil(Math.random() * max);
     console.log("you rolled", roll);
+    currentPlayer = players[currentPlayerTurn];
+    currentPlayer.position += roll;
+    console.log(currentPlayer);
+    renderBoard();
 };
+
+//create the board
+const width = 5;
+const height = 6;
+const board = [];
+
+for (var y = 0; y < height; y++) {
+    let row = [];
+    let position = 0;
+    board.push(row);
+    for (var x = 0; x < width; x++) {
+        row.push({ x, y, occupied: null, position });
+        position++;
+    }
+}
+
+console.log(board);
+const boardSizeConst = 120;
+//render Gameboard
+const renderBoard = () => {
+    let boardHTML = "";
+    board.forEach((row) => {
+        row.forEach((square) => {
+            boardHTML += `<div class="square" style="top:${
+        square.y * boardSizeConst
+      }px; left:${square.x * boardSizeConst}px;"></div>`;
+        });
+    });
+
+    document.getElementById("board").innerHTML = boardHTML;
+    console.log("render board!");
+};
+renderBoard();
+
+//players
+const players = [{
+    user: player.name,
+    position: 0,
+}, ];
+
+let currentPlayerTurn = 0;
