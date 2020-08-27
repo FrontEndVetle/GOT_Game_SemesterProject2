99c773baf28b2f8ff7ce21d3ae6abd97d857ee10
Eilert Vetle Forland <ev.forland@gmail.com>
2020-08-27 14:22:31 +0200

    The 2 characters i local storage gets retrived and populated in the DOM with the game.js. added a roll dice button which randomly gives a number between 1-6 in game.js

--- js/game.js
+++ js/game.js
@@ -1,6 +1,40 @@
-function hello() {
-    var user = JSON.parse(localStorage.getItem("Player1"));
-    console.log(typeof user);
-    console.log(user);
+function createPlayer() {
+    var player = JSON.parse(localStorage.getItem("player"));
+    console.log(player);
+
+    let playerDisplay = document.querySelector(".player");
+    playerDisplay.innerHTML += `
+    <div class="card cards">
+    <img class="card-img-top cards__img" src="${player.banner}" alt="Card image cap">
+    <div class="card-body">
+                <h5 class="card-title cards__title">${player.name}</h5>
+                  <ul class="list-group list-group-flush">
+                  </div>
+                        </div>
+                        <img class="card-img-top cards__img" src="${player.token}" alt="player token">`;
 }
-hello();
+createPlayer();
+
+function createCom() {
+    var com = JSON.parse(localStorage.getItem("com"));
+    console.log(com);
+
+    let comDisplay = document.querySelector(".com");
+    comDisplay.innerHTML += `
+    <div class="card cards">
+    <img class="card-img-top cards__img" src="${com.banner}" alt="Card image cap">
+    <div class="card-body">
+                <h5 class="card-title cards__title">${com.name}</h5>
+                  <ul class="list-group list-group-flush">
+                  </div>
+                        </div>
+                        <img class="card-img-top cards__img" src="${com.token}" alt="player token">`;
+}
+createCom();
+
+//dice roll
+window.rollDice = () => {
+    const max = 6;
+    const roll = Math.ceil(Math.random() * max);
+    console.log("you rolled", roll);
+};
